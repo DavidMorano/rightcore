f0007efdc8bbaa33cb4a181cd6c28538785ace9d
David A.D. Mornao <david.a.morano@gmail.com>
2017-10-30 14:12:34 -0400

    removal of programs whose function has been absortbed into a more general one

--- pcsname/makedate.c
+++
@@ -1,2 +0,0 @@
-const char pcsuserinfo_makedate[] =
-"@(#)pcsuserinfo             170107_1801:43_USEST Rightcore Network Services¹" ;
--- pcsname2/makedate.c
+++
@@ -1,2 +0,0 @@
-const char pcsname_makedate[] =
-	"@(#)pcsname                 111016_2207:49_EDT RightCore" ;
