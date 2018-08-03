85d80a437adfea55b757c8abf7eaa63b0cf7d7ae
David A.D. Morano <david.a.morano@gmail.com>
2018-08-03 14:01:23 -0400

    reverse stupid mistake

--- libuc/uprogdata.c
+++ libuc/uprogdata.c
@@ -197,10 +197,11 @@ int uprogdata_init()
 	    if (rs < 0)
 	        uip->f_init = FALSE ;
 	} else {
-	    while ((rs >= 0) && (! uip->f_initdone)) {
-		rs = msleep(1) ;
-		if (rs == SR_INTR) rs = SR_OK ;
+	    while ((rs >= 0) && uip->f_init && (! uip->f_initdone)) {
+	        rs = msleep(1) ;
+	        if (rs == SR_INTR) rs = SR_OK ;
 	    }
+	    if ((rs >= 0) && (! uip->f_init)) rs = SR_LOCKLOST ;
 	}
 	return (rs >= 0) ? f : rs ;
 }
