9052d5ebe8ffa6f3aed5c17524e547214aa0c69a
David A.D. Morano <david.a.morano@gmail.com>
2018-10-03 18:18:40 -0400

    fixed a bad error

--- libuc/uc_unlockpt.c
+++ libuc/uc_unlockpt.c
@@ -1,22 +1,25 @@
 /* uc_unlockpt */
 
-/* interface component for UNIX® library-3c */
+/* interface component for UNIXÂ® library-3c */
 /* unlock a PTS-type pseudo-terminal */
 
 
 /* revision history:
 
-	= 1998-03-01, David A­D­ Morano
+	= 1998-03-01, David AÂ­DÂ­ Morano
 	This subroutine was adapted from a previously related subroutine.
+
+	= 2018-10-03, David A.D. Morano
+	Fixed a bad error.
 
 */
 
-/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Copyright Â© 1998,2018 David AÂ­DÂ­ Morano.  All rights reserved. */
 
 /*******************************************************************************
 
         This subroutine unlocks a PTS-type pseudo-terminal on System V Release 3
-        (SVR3) UNIX® OS systems for use.
+        (SVR3) UNIXÂ® OS systems for use.
 
 
 *******************************************************************************/
@@ -24,22 +27,11 @@
 
 #include	<envstandards.h>	/* MUST be first to configure */
 
-#include <sys/types.h>
-#include <sys/param.h>
-#include <sys/mkdev.h>
-#include <sys/stream.h>
-#include <sys/stropts.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <sys/ptms.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <string.h>
+#include	<sys/types.h>
+#include	<stdlib.h>
 #include	<errno.h>
 
 #include	<vsystem.h>
-
 
 
 /* local defines */
@@ -56,10 +48,9 @@ int uc_unlockpt(int fd)
 	int		rs ;
 
 	errno = 0 ;
-	if ((rs = grantpt(fd)) < 0) rs = (- errno) ;
+	if ((rs = unlockpt(fd)) < 0) rs = (- errno) ;
 
 	return rs ;
 }
 /* end subroutine (uc_unlockpt) */
-
 
