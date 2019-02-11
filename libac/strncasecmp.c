387245cd80584ecdd99148632bca4bbcba4b2083
David A.D. Morano <david.a.morano@gmail.com>
2019-02-11 17:47:51 -0500

    small pretty

--- libac/strncasecmp.c
+++ libac/strncasecmp.c
@@ -1,29 +1,34 @@
 /* strncasecmp */
 
+/* knock-off of |strncasecmp(3c)| is the system does not have one */
+
+/* revision history:
+
+	= 1997-02-17, David A.D. Morano
+	Originally written for some systems without this.
+
+*/
+
+/* Copyright (c) 2997 David A.D. Morano. All rights reserved. */
+
 
 #include	<envstandards.h>
 #include	<char.h>
 
 
+/* exported subroutines */
 
-int strncasecmp(s1,s2,n)
-char	*s1, *s2 ;
-int	n ;
+
+int strncasecmp(const char *s1,const char *s2,int n)
 {
-	int	i = n ;
-
-	while ((n > 0) && *s1 && *s2) {
-
-	    if (CHAR_TOLC(*s1) != CHAR_TOLC(*s2)) 
-		return 1 ;
-
+	int		rc = 0 ;
+	while ((n > 0) && *s1 && *s2 && (rc == 0)) {
+	    rc = (CHAR_TOLC(*s1) - CHAR_TOLC(*s2)) ;
 	    s1 += 1 ;
 	    s2 += 1 ;
 	    n -= 1 ;
-
 	} /* end while */
-
-	return (*s1 == *s2) ? 0 : n ;
+	return rc ;
 }
 /* end subroutine (strncasecmp) */
 
